e2f02ee0a73d071d2bac7177837513a50eb6ac33
KovalUser19 <134967853+KovalUser19@users.noreply.github.com>
2023-09-26 13:47:27 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,19 @@
-const categories = document.querySelector("#categories");
+/* const categories = document.querySelector("#categories");
 const categoriesChildren = Array.from(categories.children);
 console.log(`Number of categories: ${categoriesChildren.length}`);
-
 
 categoriesChildren.forEach(elem => {
   console.log(`Category: ${elem.querySelector("h2").textContent}`);
   console.log(`Elements: ${elem.querySelector("ul").children.length}`);
 }
+); */
 
-);
+const categories = document.querySelector("#categories");
+const categoriesChildren = Array.from(categories.children);
+
+console.log(`Number of categories: ${categoriesChildren.length}`);
+
+ categoriesChildren.forEach(elem => {
+  console.log(`Category: ${elem.firstElementChild.textContent}`);
+  console.log(`Elements: ${elem.lastElementChild.children.length}`);
+ });
